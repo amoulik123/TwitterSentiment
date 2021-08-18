24044f259201fee90132bc9b393407edfaa2895b
Aroosh Moulik <77755747+amoulik123@users.noreply.github.com>
2021-08-18 18:36:43 -0500

    Delete secrets.py

--- secrets.py
+++
@@ -1,4 +0,0 @@
-consumer_key="j4ZCm6UA0GuB4x2DWSYjNWeNO"
-consumer_secret="B3AlM83AwHm3mPjJLLkQWKyYF9vb4clthIm2cKm5UNnz1LOnVa"
-access_token_key="2940346832-0P1O03ZtzMHQOUzUKLfZKMLDjqfA1hnnmGml3iq"
-access_token_secret="IJk4aIiOJ4pHwq9sQS3IuoIPYFZOtHi0hWX7Rh1DgFLka"
